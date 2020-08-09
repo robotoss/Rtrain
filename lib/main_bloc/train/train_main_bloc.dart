765e83ad117b4f79d81ffc9da8193f487d22acf0
FreeOn <zoxo@outlook.com>
2020-08-09 18:21:13 +0600

    Update project

--- lib/main_bloc/train/train_main_bloc.dart
+++ lib/main_bloc/train/train_main_bloc.dart
@@ -10,8 +10,13 @@ part 'train_main_state.dart';
 class TrainMainBloc extends Bloc<TrainMainEvent, TrainMainState> {
   TrainMainBloc() : super(TrainMainInitial());
 
-  Pedometer _pedometer;
-  StreamSubscription<int> _subscription;
+  // Pedometer _pedometer;
+  // StreamSubscription<int> _subscription;
+
+  Stream<StepCount> _stepCountStream;
+  Stream<PedestrianStatus> _pedestrianStatusStream;
+
+  int steps;
 
   @override
   Stream<TrainMainState> mapEventToState(
@@ -23,13 +28,27 @@ class TrainMainBloc extends Bloc<TrainMainEvent, TrainMainState> {
   }
 
   Stream<TrainMainState> _buildStartWorkoutEvent() async* {
-    _pedometer = new Pedometer();
-    _subscription = _pedometer.pedometerStream.listen((int stepCountValue) {
-      print(stepCountValue);
-    }, onError: (error) {
-      print("Flutter Pedometer Error: $error");
-    }, onDone: () {
-      print("Finished pedometer tracking");
-    }, cancelOnError: true);
+    // _pedometer = new Pedometer();
+    // _subscription = _pedometer.pedometerStream.listen((int stepCountValue) {
+    //   print(stepCountValue);
+    // }, onError: (error) {
+    //   print("Flutter Pedometer Error: $error");
+    // }, onDone: () {
+    //   print("Finished pedometer tracking");
+    // }, cancelOnError: true);
+
+    _pedestrianStatusStream = await Pedometer.pedestrianStatusStream;
+    _stepCountStream = await Pedometer.stepCountStream;
+
+    /// Listen to streams and handle errors
+    _stepCountStream.listen((event) {
+      steps = event.steps;
+      DateTime timeStamp = event.timeStamp;
+    }).onError((error) {});
+
+    _pedestrianStatusStream.listen((event) {
+      String status = event.status;
+      DateTime timeStamp = event.timeStamp;
+    }).onError((error) {});
   }
 }
